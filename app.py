dbb3228d6a7dea143065c5eea05bbb05ba00d06b
Aditya <athemastermind9@gmail.com>
2023-04-23 18:03:01 +0530

    First Commit ChatBot

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/message', methods=['POST'])
+def message():
+    message = request.json['message']
+    response = requests.post('http://localhost:5005/webhooks/rest/webhook', json={"message": message})
+    return jsonify(response.json())
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
